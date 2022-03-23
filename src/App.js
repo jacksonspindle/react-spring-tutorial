c75155bf86498dd4238d8c514d5896d0b5e59b85
Jackson Spindle <jacksonmilesspindle@gmail.com>
2022-03-23 00:01:24 -0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ function Cube(props){
     <animated.mesh ref={mesh}
       onPointerOver={(event)=> setHover(true)}
       onPointerOut={(event)=>setHover(false)}
-      onClick={(event)=> setActive(!active)}
+      // onClick={(event)=> setActive(!active)}
+      
       scale={ scale }
       {...props}
       >
